f92dd3460a16c0464ea9f45cc2a47cb1365d7e43
johnLamberts <faithsacredoo3@gmail.com>
2024-11-08 18:19:37 +0800

    feat: get user added to the core

--- src/modules/user/user.controllers.ts
+++ src/modules/user/user.controllers.ts
@@ -1,6 +1,8 @@
 import { HttpStatusCodes } from "@/constants";
+import { TPaginationResponse } from "@/interfaces";
 import { customResponse } from "@/utils";
 import { NextFunction, Request, Response } from "express";
+import { IUser } from "./user.interface";
 import UserService from "./user.service";
 
 export class UserController {
@@ -44,4 +46,49 @@ export class UserController {
 
     }
   }
+
+  getUsersHandler = async (_req: Request, res: TPaginationResponse) => {
+    try {
+      if(res.paginatedResults) {
+        const { results, next, previous, currentPage, totalDocs, totalPages, lastPage } = res.paginatedResults;
+  
+        const responseObject: any = {
+          totalDocs: totalDocs || 0,
+          totalPages: totalPages || 0,
+          lastPage: lastPage || 0,
+          count: results?.length || 0,
+          currentPage: currentPage || 0,
+        };
+  
+        if(next) {
+          responseObject.nextPage = next;
+        }
+        if(previous) {
+          responseObject.prevPage = previous;
+        }
+  
+        responseObject.users = results?.map((user: IUser) => {
+          return {
+            ...user,
+            request: {
+              type: 'GET',
+              description: 'Get one phamarcy with the ID',
+              url: `http://localhost:8080/api/v1/user/${user.user_uid}`
+            }
+          }
+        })
+  
+        const success = customResponse<typeof responseObject>().success(HttpStatusCodes.OK, responseObject, "Successfully found medicines");
+  
+        return res.status(success.statusCode).json(success)
+      } else {
+        const error = customResponse().error(404, new Error('No medicine found.'), "No medicine found")
+        return res.status(error.statusCode).json(error)
+      }
+    } catch (error) {
+      return res.status(500).send(
+        customResponse().error(404, error as Error, 'An error occurred while retrieving medicine')
+      )
+    }
+  } 
 }
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,3 +1,4 @@
+import { usersFeature } from "@/common/middlewares/sorter-filter";
 import express from "express";
 import { UserController } from "./user.controllers";
 
@@ -8,7 +9,7 @@ const userController = new UserController;
 
 // router.get("/", UserController.getUsersHandler)
 router.post("/add_user", (userController as any).addUserHandler)
-// router.get("/", usersFeature(), userController.getStudentsHandler as any)
+router.get("/", usersFeature(), userController.getUsersHandler as any)
 // router.get('/api/v1/user/:id', (userController as any).getOneUserHandler);
 
 
